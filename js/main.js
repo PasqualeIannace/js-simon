51203a3bd55580879975162a7732a07d149634d9
PasqualeIannace <pasquale.iannace@gmail.com>
2023-11-09 23:08:02 +0100

    inserita funzione verifica comparazione numeri

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const arrNumeri = [];
+const myNumbers = [];
 randomNumbers();
 
 // genero i 5 numeri casuali
@@ -6,6 +7,7 @@ function randomNumbers() {
     for(i = 0; i < 5; i++) {
         arrNumeri.push(Math.floor(Math.random() * 100) + 1);
     }
+    console.log("numeri nascosti", arrNumeri);
 }
 
 // nascondo i numeri
@@ -13,7 +15,58 @@ function hideNumbers() {
     document.getElementById("numeri").innerHTML = "";
 }
 
+// chiedo i numeri all'utente
+function askNumbers() {
+    for(i = 0; i < 5; i++) {
+        myNumbers.push(parseInt(prompt("Che numero hai visto?")));
+    }
+    // pusho gli array al confronto
+    checkDifference(arrNumeri, myNumbers);
+}
+
+// verifico i numeri random con quelli dell'utente
+function checkDifference(mathArray, mioArray) {
+
+    const numeriErrati = [];
+    const numeriCorretti = [];
+
+    let counter = 0;
+    while(counter < mathArray.length) {
+        numDaVerificare = mioArray[counter];
+        
+
+        if(mathArray.indexOf(numDaVerificare) === -1) {
+            console.log("Numeri errati", numDaVerificare);
+            numeriErrati.push(numDaVerificare);
+
+            counter++;
+        } else {
+            console.log("Trovato doppione", numDaVerificare);
+            numeriCorretti.push(numDaVerificare);
+            counter++;
+        }
+
+        console.log("Array errori:", numeriErrati);
+        console.log("Array numeri corretti:", numeriCorretti);
+    }
+
+
+    // for(i = 0; i < mathArray.length; i++) {
+
+    //     // if(mathArray.indexOf(mioArray) === -1) {
+    //     //     if(mathArray[i] == mioArray[i])
+    //     //     console.log("Trovato math", mathArray[i]);
+    //     //     console.log("Trovato mio", mioArray[i]);
+    //     // } else {
+    //     //     console.log("NON TROVATO", mathArray[i]);
+
+    //     // }
+    // }
+}
+
 document.getElementById("numeri").innerHTML = arrNumeri;
 
 // invoco nascondi numeri dopo 30 secondi
-setTimeout(hideNumbers, 30*1000);
+// setTimeout(hideNumbers, 30*1000);
+setTimeout(hideNumbers, 2*1000);
+setTimeout(askNumbers, 2*1010);
